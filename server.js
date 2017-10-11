ab5f349ec1f0c6d5108ad8652a4438d31a6ad9df
frank <18652171101@163.com>
2017-10-11 20:57:11 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+/**
+ * Created by frank on 2017/10/11.
+ */
+
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const Router = require('koa-router')
+const router = new Router()
+
+const app = new Koa()
+
+app.use(bodyParser())
+
+router.get('/ping', ctx => {
+  'use strict'
+  ctx.body = 'pong'
+})
+
+router.post('/pwd', ctx => {
+  const {host} = ctx.request.body
+  ctx.body = {host}
+})
+app.use(router.routes()).use(router.allowedMethods())
+app.listen(3000)
